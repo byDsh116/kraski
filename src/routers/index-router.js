d8e21bded43cf238fab62a416cd165e0d0afca43
byDsh116 <by.dsh116@gmail.com>
2024-01-09 18:37:56 +0300

    add styles, add get routes, changed navbar

---
+++ src/routers/index-router.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const renderTemplate = require('../lib/renderTemplate');
+const Blog = require('../views/pages/Blog');
+const Catalog = require('../views/pages/Catalog');
+const Contacts = require('../views/pages/Contacts');
+const Delivery = require('../views/pages/Delivery');
+const Main = require('../views/pages/Main');
+const Vacancies = require('../views/pages/Vacancies');
+
+router.get('/', async (req, res) => {
+  renderTemplate(Main, {}, res);
+});
+
+router.get('/catalog', async (req, res) => {
+  renderTemplate(Catalog, {}, res);
+});
+
+router.get('/delivery', async (req, res) => {
+  renderTemplate(Delivery, {}, res);
+});
+
+router.get('/contacts', async (req, res) => {
+  renderTemplate(Contacts, {}, res);
+});
+
+router.get('/vacancies', async (req, res) => {
+  renderTemplate(Vacancies, {}, res);
+});
+
+router.get('/blog', async (req, res) => {
+  renderTemplate(Blog, {}, res);
+});
+
+module.exports = router;
--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const Navbar = require('./components/Navbar');
-// const NavbarTop = require('./components/NavbarTop');
+const NavbarTop = require('./components/NavbarTop');
 
 module.exports = function Layout({ children }) {
     return (
@@ -12,11 +12,11 @@ module.exports = function Layout({ children }) {
                 <link rel="stylesheet" href="css/style.css" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
                 {/* <script defer src="./js/application.js" type="text/javascript" /> */}
-                <title>Краски-студия</title>
+                <title>Krapiva</title>
             </head>
 
             <body>
-                {/* <NavbarTop /> */}
+                <NavbarTop />
                 <Navbar />
                 <div> {children} </div>
                 <div>
