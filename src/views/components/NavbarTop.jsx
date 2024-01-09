a71b028097b42e107e4ccc172e85d7e6a2adf319
byDsh116 <by.dsh116@gmail.com>
2024-01-09 16:22:43 +0300

    add logo in navbar, navbar top

---
+++ src/views/components/NavbarTop.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+
+module.exports = function NavbarTop() {
+    return (
+        <nav class="nav nav-pills nav-justified">
+
+            <p className='adres'>Адрес шоурума: Москва, Пятницкая 54 стр 1</p>
+            <span className="topbar-work-time">Ежедневно с 9:00 до 21:00</span>
+            <a href="tel:+7(925)200-63-75‬" className="topbar-phone">+7 (925) 200-63-75‬</a>
+
+        </nav>
+    )
+}
+
+
+
