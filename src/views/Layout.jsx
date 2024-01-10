49a783e77ae09e97ded414171942a12e809beec4
byDsh116 <by.dsh116@gmail.com>
2024-01-11 01:46:57 +0300

    add footer

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -26,7 +26,7 @@ module.exports = function Layout({ children }) {
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></script>
-                <script type="text/javascript" charSet="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aaf964574c907ff73d89cf1cc7a893e86d25eb8944ff264f003faf68c8b29fe36&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                <script type="text/javascript" charSet="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aaf964574c907ff73d89cf1cc7a893e86d25eb8944ff264f003faf68c8b29fe36&amp;width=400&amp;height=300&amp;lang=ru_RU&amp;scroll=true"></script>
 
                 {/* <scrip src="/js/script.js" /> */}
             </body>
---
+++ src/views/components/Footer.jsx
@@ -0,0 +1,10 @@
+const React = require('react')
+const Layout = require('../Layout')
+
+module.exports = function Footer() {
+    return (
+
+        <h1>footr</h1>
+
+    )
+}
