c7ab2b133ee513908dceee960de9a6aa86bbafc3
byDsh116 <by.dsh116@gmail.com>
2024-01-10 21:14:07 +0300

    add user icon, db, fetch for reg

--- public/js/reg.js
+++ public/js/reg.js
@@ -1,6 +1,7 @@
 console.log('reg-form');
 
 const form = document.querySelector('.reg-form');
+const homeDiv = document.querySelector('.home');
 console.log(form);
 
 form.addEventListener('submit', async (e) => {
@@ -10,5 +11,20 @@ form.addEventListener('submit', async (e) => {
     [e.target.email.name]: e.target.email.value,
     [e.target.password.name]: e.target.password.value,
   };
-  //   const respons = await fetch
+  const response = await fetch('http://localhost:3000/user/reg', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  });
+  if (response.status === 200) {
+    window.location.href = ' http://localhost:3000/';
+    // const result = await response.json;
+    // const succesDiv = document.createElement('div');
+    // succesDiv.innerHTML = `
+    //   <h1> ${result.name} you are succes registration</h1>
+    //   `;
+    // homeDiv.appendChild(succesDiv);
+  }
 });
